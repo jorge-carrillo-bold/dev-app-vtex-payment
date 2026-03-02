1de3a165b733ae6ad2894f2006343f8a691f0ef7
Jorge Carrillo <jorge.carrillo@bold.co>
2026-03-02 16:01:18 -0500

    Adjust point to QA

--- node/index.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentProviderService } from '@vtex/payment-provider'
-
-import TestSuiteApprover from './connector'
-
-export default new PaymentProviderService({
-  connector: TestSuiteApprover,
-})
