6e1138ada3d7d96351e52624767a3cce18d86e06
Jorge Carrillo <jorge.carrillo@bold.co>
2026-03-03 15:42:11 -0500

    Ajuste sin node

--- node/clients/index.ts
+++
@@ -1,9 +0,0 @@
-import { IOClients } from '@vtex/api'
-
-import { MyPCICertifiedClient } from './SecureClient'
-
-export class CustomClients extends IOClients {
-  public get myPCIClient() {
-    return this.getOrSet('myPCIClient', MyPCICertifiedClient)
-  }
-}
--- node/utils.ts
+++
@@ -1,9 +0,0 @@
-export const randomString = () => {
-  return Math.random()
-    .toString(36)
-    .substring(7)
-}
-
-export const randomUrl = () => {
-  return `https://${randomString()}.com`
-}
